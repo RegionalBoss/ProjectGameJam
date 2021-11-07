aa590334d9fe488981784962bad3f9415a0a8934
Karbovanec Eduard <karbonat.uah@gmail.com>
2021-11-07 08:46:06 +0100

    new fonts

--- Assets/Scripts/IntroScript.cs
+++ Assets/Scripts/IntroScript.cs
@@ -19,8 +19,8 @@ namespace ProjectGameJam
     void Start()
     {
       List<DialogText> text = new List<DialogText>();
-      text.Add(new DialogText("Long-forgotten legends speak of magical scrolls, used to seal away beings we couldn’t understand or even comprehend.", 12));
-      text.Add(new DialogText("I spent my whole life studying this mystery, and over time, darkness crept into my mind. After years of searching and nightmares, I managed to find one of these scrolls in an abandoned mansion.", 15));
+      text.Add(new DialogText("Long-forgotten legends speak of magical scrolls, used to seal away beings we couldn’t understand or even comprehend.", 11));
+      text.Add(new DialogText("I spent my whole life studying this mystery, and over time, darkness crept into my mind. After years of searching and nightmares, I managed to find one of these scrolls in an abandoned mansion.", 18));
       text.Add(new DialogText("But the voice that guided me to it keeps calling me still, pleading for help, and the visions keep getting worse. ", 15));
       text.Add(new DialogText("I tried getting rid of the scroll, only to find it back in my pocket. It seems I am tethered to it… for better or worse.", 15));
       Debug.Log(text + " " + audioClip.ToString());
